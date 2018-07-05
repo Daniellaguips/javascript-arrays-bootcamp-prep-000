d0f2afd239ceca0a0cce47d720cfcb8af12490ca
daniellaguips@gmail.com Guimaraes <daniellaguips@gmail.com>
2018-07-05 19:53:25 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -22,7 +22,7 @@ function accessElementInArray(array, index){
   return array[index]
 }
 
-function destructivelyRemoveElementFromBegginingOfArray(array){
+function destructivelyRemoveElementFromBeginningOfArray(array){
   array.shift()
 }
 
